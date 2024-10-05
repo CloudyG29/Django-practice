9e1cf62dcfa3c6ed60fe0dd5e37a95891433ec1c
CloudyG29 <ngmulaudzi2@gmail.com>
2024-10-05 12:05:45 +0200

    changed index url

--- practice/urls.py
+++ practice/urls.py
@@ -22,9 +22,10 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('store/', include("home.urls")),
+    path('', include("home.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatterns += [
-    path('', RedirectView.as_view(url='store/', permanent=True)),
-]
+
+#urlpatterns += [
+#    path('', RedirectView.as_view(url='store/', permanent=True)),
+#]

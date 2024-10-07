5eed54637cb165572c471c65b5503e1926f1b80b
CloudyG29 <ngmulaudzi2@gmail.com>
2024-10-07 09:27:37 +0200

    create superuser

--- practice/deployment.py
+++ practice/deployment.py
@@ -33,3 +33,14 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 DATABASES = {
     'default': dj_database_url.config(default=os.getenv('DATABASE_URL'), conn_max_age=600)
 }
+
+
+if os.environ.get('CREATE_SUPERUSER'):
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+    if not User.objects.filter(username=os.environ.get('SUPERUSER_NAME')).exists():
+        User.objects.create_superuser(
+            username=os.environ.get('SUPERUSER_NAME'),
+            email=os.environ.get('SUPERUSER_EMAIL'),
+            password=os.environ.get('SUPERUSER_PASSWORD')
+        )
